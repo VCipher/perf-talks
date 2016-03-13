3494b26eafc2b41b4631fe4d31efa895fdddb15e
Grigoriy <violentcipher@gmail.com>
2016-03-13 17:46:12 +1000

    add samples

---
+++ samples/ArrayVsLinkedList/ArrayVsLinkedList/Program.cs
@@ -0,0 +1,56 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+
+namespace ArrayVsLinkedList
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var summary = BenchmarkRunner.Run<ArrayVsLinkedList>();
+        }
+    }
+
+    public class ArrayVsLinkedList
+    {
+        private const int N = 1000000;
+
+        private readonly int[] _arr = new int[N];
+        private readonly LinkedList<int> _linked = new LinkedList<int>();
+
+        public ArrayVsLinkedList()
+        {
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            for (int i = 0; i < N; i++)
+            {
+                int res = rnd.Next();
+
+                _arr[i] = res;
+                _linked.AddLast(res);
+            }
+        }
+
+        [Benchmark]
+        public void Array()
+        {
+            int sum = 0;
+            for (int curr = 0; curr < _arr.Length; ++curr)
+            {
+                sum += _arr[curr];
+            }
+        }
+
+        [Benchmark]
+        public void LinkedList()
+        {
+            int sum = 0;
+            for (LinkedListNode<int> curr = _linked.First; curr != null; curr = curr.Next)
+            {
+                sum += curr.Value;
+            }
+        }
+    }
+}
+
---
+++ samples/MemoryLeak/MemoryLeak/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace MemoryLeak
+{
+    public class Scheduler
+    {
+        public event EventHandler Cancel;
+    }
+
+    public class Worker
+    {
+        private byte[] _data;
+        private Scheduler _scheduler;
+
+        public Worker(Scheduler scheduler)
+        {
+            _data = new byte[1024 * 100];
+            _scheduler = scheduler;
+
+            _scheduler.Cancel += Scheduler_Cancel;
+        }
+
+        public void Work()
+        {
+            Thread.Sleep(50);
+        }
+
+        private void Scheduler_Cancel(object sender, EventArgs e)
+        {
+            Console.WriteLine("I'm canceled");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scheduler scheduler = new Scheduler();
+
+            while (true)
+            {
+                new Worker(scheduler).Work();
+            }
+        }
+    }
+}
